bc2980b0f0aaebc60a92d3672b2ae3a6a3eef283
jerrodq2 <jerrodq2@yahoo.com>
2016-12-28 19:58:49 -0600

    Update manage.py
    attempt to fix no module named flask.ext.script error

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 
     Define terminal commands here to run actions
 """
-from flask.ext.script import Manager, Server
+from flask_script import Manager, Server
 from flask.ext.sqlalchemy import SQLAlchemy
 # from flask.ext.migrate import Migrate, MigrateCommand
 from system.init import initialize_app
